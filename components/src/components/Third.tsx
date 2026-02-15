0d6eb51aca968d5ed39807cb940bc3201c24f55e
onesailoriginal <kopocsy.bence.mate.21i@egressy.info>
2026-02-15 15:09:17 +0100

    Backend hozzáírva

--- components/src/components/Third.tsx
+++ components/src/components/Third.tsx
@@ -7,7 +7,7 @@ function Third(){
     const [content, setContent] = useState<Props | null>(null)
     async function serverConnect(){
         try{
-            const req = await fetch('http://127.1.1.1:300/loadDatas')
+            const req = await fetch('http://127.1.1.1:3000/loadDatas')
             if(!req.ok){
                 throw new Error(`Szerver hiba: ${req.status}`)
             }
